511d119c06c5f161ba13c3b5f3d9f0bf25103d4e
Martin <m87.wheeler@gmail.com>
2020-06-22 15:06:11 +0100

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use("/", express.static(path.join(__dirname, "client")));
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
